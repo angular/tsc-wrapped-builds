2747be596e64974ea7ff7d26525f2b7d3c40bb8b
GitHub <noreply@github.com>
2016-11-22 23:15:43 +0000

    86ffa88 fix(build): update versions of umd bundles (#13038)

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 var compiler_host_1 = require('./src/compiler_host');
+exports.DecoratorDownlevelCompilerHost = compiler_host_1.DecoratorDownlevelCompilerHost;
 exports.MetadataWriterHost = compiler_host_1.MetadataWriterHost;
-exports.TsickleHost = compiler_host_1.TsickleHost;
 var main_1 = require('./src/main');
 exports.main = main_1.main;
 __export(require('./src/cli_options'));
